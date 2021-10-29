036fe014fa432898b6deda1ac4f081b0f377b42e
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2021-10-29 14:47:16 -0400

    Organizando layout com a referência

---
+++ projeto-f4life-kahena/index.js
@@ -0,0 +1,14 @@
+//function recebePalavra(palavra) {
+//     if (palavra[index] == 'a' || 'e' || 'i' || 'o' || 'u') {
+//         for (let index = 0; index < palavra.length; index++) {
+//         palavra[index] == 'a' || 'e' || 'i' || 'o' || 'u'
+//         console.log(palavra[index])
+//         }
+//     } else {
+//         for (let index = 0; index < palavra.length; index++) {
+//             palavra[index] !== 'a' || 'e' || 'i' || 'o' || 'u'
+//             console.log(palavra[index])
+//         }
+// }
+
+// recebePalavra(alvo)
